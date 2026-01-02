713709fdc2450e97dbfe837d0c07f51a35f1104d
Kevin Carrasco <itzvin19@gmail.com>
2026-01-02 16:03:14 -0500

    instalacion de astro collections

---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+import { defineCollection } from "astro:content";
+import { z } from "zod";
+
+const blogCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    description: z.string(),
+    image: z.string(),
+
+    author: z.string(),
+    tags: z.array(z.string()),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};

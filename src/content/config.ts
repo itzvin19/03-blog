1a0804b78c62ddaa9f4a55a750b8482010bfadc3
Kevin Carrasco <itzvin19@gmail.com>
2026-01-03 14:24:21 -0500

    astro collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,35 @@
-import { defineCollection } from "astro:content";
+import { defineCollection, reference } from "astro:content";
 import { z } from "zod";
 
 const blogCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    date: z.date(),
-    description: z.string(),
-    image: z.string(),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string(),
+      image: image(),
+      author: reference("author"),
+      tags: z.array(z.string()),
+      isDraft: z.boolean().default(false),
+    }),
+});
 
-    author: z.string(),
-    tags: z.array(z.string()),
-  }),
+const authorCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      avatar: image(),
+      twitter: z.string(),
+      linkedIn: z.string(),
+      github: z.string(),
+      bio: z.string(),
+      subtitle: z.string(),
+    }),
 });
 
 export const collections = {
   blog: blogCollection,
+  author: authorCollection,
 };
